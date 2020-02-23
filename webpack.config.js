adfec6368367049ae1e4bfd8848067c83cc3d5e7
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2020-02-23 21:50:44 +1100

    Fix incorrect paths

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: '.blog/src/index.js',
+  entry: './blog/src/index.js',
   output: {
     path: path.resolve(__dirname, 'blog/static'),
     filename: 'js/bundle.js'
