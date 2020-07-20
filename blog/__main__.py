6b8bce30e933ccd2409902442f0978d450695a31
Hayley Hughes <hayley@foxes.systems>
2020-07-20 15:40:31 +1000

    Migrate to github pages

--- blog/__main__.py
+++ blog/__main__.py
@@ -1,7 +1,7 @@
 from flask_frozen import Freezer
 from blog import app
 
-app.config['FREEZER_DESTINATION'] = '../public'
+app.config['FREEZER_DESTINATION'] = '../docs'
 app.config['FREEZER_RELATIVE_URLS'] = False
 app.config['FREEZER_REMOVE_EXTRA_FILES'] = False
 
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pipenv run python3 -m blog
-
-cd public
-
-git add *
-git commit -m $(date +%s)
-git push origin master
